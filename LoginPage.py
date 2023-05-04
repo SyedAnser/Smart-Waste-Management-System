838b531380a7df0fa0810436dbfb48eac4842b0f
Blue4819 <88502369+Blue4819@users.noreply.github.com>
2023-05-05 04:15:30 +0530

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -1,9 +1,14 @@
+import requests
+from timeit import default_timer as timer
+import cProfile
+import re
+from pympler import tracker
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.card import MDCard
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.list import TwoLineIconListItem, IconLeftWidget
-from kivymd.uix.button import MDIconButton
+from kivymd.uix.button import MDIconButton, MDFloatingActionButton
 from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
@@ -20,10 +25,17 @@ from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationIt
 from kivymd.uix.boxlayout import MDBoxLayout
 import mysql.connector as mysql
 
+start = timer()
+
+cProfile.run('re.compile("foo|bar")')
+
+tr = tracker.SummaryTracker()
+tr.print_diff()
+
 mydb = mysql.connect(
   host="localhost",
   user="root",
-  password="SA9310189666!",
+  password="password",
   database="CleanBee"
 )
 mycursor = mydb.cursor()
@@ -37,14 +49,13 @@ class WindowManager(ScreenManager):
 Window.size = (590, 600)
 
 class LoginInterface(Screen):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.app = MDApp.get_running_app()
 
     def forgot_password(self):
         self.manager.current = 'reset_password'
-    
+
     def login(self):
 
         username = self.ids.login_username.text
@@ -139,22 +150,22 @@ class ResetPasswordPage(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.app = MDApp.get_running_app()
-    
+
     def reset_password(self):
         # Get the values entered by the user
         username = self.ids.txt_username.text
         password = self.ids.txt_password.text
         confirm_password = self.ids.txt_confirm_password.text
-        
+
         # Check if the username exists in the database
         cnx = mysql.connect(user='root', password='SA9310189666!',
-                                      host='localhost',
-                                      database='CleanBee')
+                            host='localhost',
+                            database='CleanBee')
         cursor = cnx.cursor()
         query = ("SELECT * FROM user WHERE username = %s")
         cursor.execute(query, (username,))
         user = cursor.fetchone()
-        
+
         if user is None:
             cancelbtn = MDFlatButton(text='Retry', on_release=self.close_dialogue)
             self.dialog = MDDialog(title="User Not Found", text="The given username is not registered with us",
@@ -176,20 +187,16 @@ class ResetPasswordPage(Screen):
             self.dialog = MDDialog(title="Password Updated", text="Password has been updated successfully!",
                                    size_hint=(0.7, 0.2), buttons=[cancelbtn])
             self.dialog.open()
-            self.manager.current='loginpage'
+            self.manager.current = 'loginpage'
             cnx.close()
-    
+
     def close_dialogue(self, obj):
         self.dialog.dismiss()
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.overview_layout = BoxLayout(orientation="vertical", padding="10dp",pos_hint={"center_x": 0.5, "top": 1})
-        self.overview_scrollview = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
         self.app = MDApp.get_running_app()
-        self.profile_layout = BoxLayout(orientation="vertical", padding="10dp", size_hint=(None, None), size=("300dp", "300dp"), pos_hint={"center_x": 0.5, "top": 1})
-        self.edit_scrollview = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
 
     def enable_add_button(self, instance, value):
         if value:
@@ -198,40 +205,53 @@ class MainScreen(Screen):
             self.ids.button.disabled = True
 
     def on_enter(self, *args):
+        self.overview_layout = BoxLayout(orientation="vertical", padding="10dp",pos_hint={"center_x": 0.5, "top": 1})
+        self.overview_layout.clear_widgets()
+        self.overview_scrollview = ScrollView(size_hint=(1, 1), size=(Window.width, Window.height))
+        self.overview_scrollview.clear_widgets()
+        self.profile_layout = BoxLayout(orientation="vertical", padding="10dp", size_hint=(None, None), size=("300dp", "300dp"), pos_hint={"center_x": 0.5, "top": 1})
+        self.profile_layout.clear_widgets()
+        self.edit_scrollview = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        self.edit_scrollview.clear_widgets()
+        self.urgent_layout = BoxLayout(orientation="vertical", padding="10dp",pos_hint={"center_x": 0.5, "top": 1})
+        self.urgent_layout.clear_widgets()
+
         self.update_overview_layout()
         self.update_profile()
         self.update_edit_layout()
+        self.update_urgent_layout()
 
-    def showpopup(self):
+    def showpopup(self, message):
         print("function called")
-        self.content = BoxLayout(orientation = "vertical", size_hint_y=None, height=dp(40))
+        content = BoxLayout(orientation = "vertical", size_hint_y=None, height=dp(40))
         dustbin_codetf = MDTextField(id="dustbin_code", hint_text="Dustbin Code", required=True)
         dustbin_typetf = MDTextField(id="dustbin_type", hint_text="Dustbin Type (Public/Private)", required=True)
         locationtf = MDTextField(id="location", hint_text="Location", required=True)
         cancelbutton = MDFlatButton(text="Cancel", on_release=self.dismiss)
-        addbutton = MDFlatButton(text="Add", on_release=self.add_dustbin)
-        self.content.add_widget(dustbin_codetf)
-        self.content.add_widget(dustbin_typetf)
-        self.content.add_widget(locationtf)
-        self.content.add_widget(cancelbutton)
-        self.content.add_widget(addbutton)
+        addbutton = MDFlatButton(text="Add", on_release=lambda x: self.add_dustbin(dustbin_codetf,dustbin_typetf,locationtf))
+        content.add_widget(dustbin_codetf)
+        content.add_widget(dustbin_typetf)
+        content.add_widget(locationtf)
+        content.add_widget(cancelbutton)
+        content.add_widget(addbutton)
 
-        self.popup = Popup(title='Add dustbin', content=self.content)
+        self.popup = Popup(title='Add dustbin', content=content, size=(dp(300), dp(300)))
         self.popup.open()
 
-    def dismiss(self):
+    def dismiss(self,x):
         self.popup.dismiss()
 
-    def add_dustbin(self, add):
-        print("Adding")
+    def add_dustbin(self, dc,dt,l):
+        print(dc.text,dt.text,l.text)
         # Add record to Dustbin table
-        dustbin_code = int(self.ids.dustbin_code.text)
-        dustbin_type = self.ids.dustbin_type.text
-        location = self.ids.location.text
+        dustbin_code = int(dc.text)
+        dustbin_type =dt.text
+        location = l.text
 
         insert_query = "INSERT INTO Dustbin (DustbinCode, DustbinType, Location) VALUES (%s, %s, %s)"
         insert_values = (dustbin_code,dustbin_type,location)
         mycursor.execute(insert_query, insert_values)
+        mydb.commit()
 
         self.add_dustbin_status(dustbin_code,0,self.app.ccode)
 
@@ -240,16 +260,20 @@ class MainScreen(Screen):
         insert_query = "INSERT INTO DustbinStatus (DustbinCode, Status, CompanyCode) VALUES (%s, %s, %s)"
         insert_values = (dustbin_code, status, company_code)
         mycursor.execute(insert_query, insert_values)
-        self.update_overview_layout()
-        self.update_edit_layout()
-        self.update_profile()
+        mydb.commit()
+        self.on_enter()
+        self.popup.dismiss()
 
     def update_overview_layout(self):
         # Clear the existing widgets from the layout
-        self.overview_layout.clear_widgets()
+        self.ids.overview_tab.clear_widgets()
 
         mycursor.execute("SELECT * FROM DustbinStatus WHERE CompanyCode = %s", (self.app.ccode,))
         self.dustbin_data = mycursor.fetchall()
+
+        # Create a box layout to contain all dustbin data
+        dustbin_container = BoxLayout(orientation='vertical', size_hint_y=None, padding=(dp(20), dp(10)))
+        dustbin_container.bind(minimum_height=dustbin_container.setter('height'))
 
         # Loop through the dustbin data and create a text field for each entry
         for data in self.dustbin_data:
@@ -257,29 +281,33 @@ class MainScreen(Screen):
             fill_level = data[1]
 
             # Create the text field for the dustbin data
-            dustbin_textfield = MDLabel(text='Dustbin {} - Fill Level: {}% '.format(dustbin_id, fill_level*10), size_hint_y=None)
+            dustbin_textfield = MDLabel(text='Dustbin {} - Fill Level: {}% '.format(dustbin_id, fill_level * 10),
+                                        size_hint_y=None)
 
             # Create the progress bar for the dustbin data
             dustbin_progressbar = MDProgressBar(value=fill_level / 10, max=1, size_hint_x=None, width=300)
 
             # Create the box layout for the dustbin data
-            dustbin_boxlayout = MDBoxLayout(orientation='vertical', size_hint_y=None, height=dp(100),padding=(dp(20), dp(10)),pos_hint={"center_x": 0.5, "top": 1})
+            dustbin_boxlayout = MDBoxLayout(orientation='vertical', size_hint_y=None, height=dp(100),
+                                            padding=(dp(20), dp(10)), pos_hint={"center_x": 0.5, "top": 1})
 
             # Add the text field and progress bar to the box layout
             dustbin_boxlayout.add_widget(dustbin_textfield)
             dustbin_boxlayout.add_widget(dustbin_progressbar)
 
-            # Add the box layout to the edit layout
-            self.overview_layout.add_widget(dustbin_boxlayout)
+            # Add the box layout to the dustbin container
+            dustbin_container.add_widget(dustbin_boxlayout)
 
-        # Add the edit layout to the scroll view
-        self.overview_scrollview.add_widget(self.overview_layout)
+        # Create a scroll view to contain the dustbin container
+        overview_scrollview = ScrollView()
+        overview_scrollview.add_widget(dustbin_container)
 
-        # Add the scroll view to the main layout
-        self.ids.overview_tab.add_widget(self.overview_scrollview)
+        # Add the scroll view to the overview layout
+        self.ids.overview_tab.add_widget(overview_scrollview)
 
     def update_profile(self):
         # Create the profile box layout
+        self.ids.profile_tab.clear_widgets()
         profile_boxlayout = MDBoxLayout(orientation='vertical', spacing=dp(10), padding=dp(10),pos_hint={"center_x": 0.5, "top": 1})
 
         # Create and add the company name and icon
@@ -331,10 +359,42 @@ class MainScreen(Screen):
         # Add the profile layout to the profile tab
         self.ids.profile_tab.add_widget(self.profile_layout)
 
+    def update_urgent_layout(self):
+        # Clear the existing contents of the tab
+        self.ids.urgent_tab.clear_widgets()
+
+        # Get the dustbins with status >= 8 and company code equal to self.app.ccode
+        query = f"SELECT DustbinCode, Status FROM DustbinStatus WHERE Status >= 8 AND CompanyCode = '{self.app.ccode}'"
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+
+        # Create a layout for each dustbin
+        for dustbin_code, status in result:
+            layout = BoxLayout(orientation='horizontal')
+            label = MDLabel(text=f"Dustbin {dustbin_code}: {status}")
+            button = MDFlatButton(text="Empty dustbin", md_bg_color=[0, 0.8, 0.2, 1])
+            button.bind(on_press=lambda event: self.convert_to_zero(dustbin_code))
+            layout.add_widget(label)
+            layout.add_widget(button)
+
+            # Add the layout to the tab
+            self.urgent_layout.add_widget(layout)
+
+        self.ids.urgent_tab.add_widget(self.urgent_layout)
+
+    def convert_to_zero(self, code):
+        query = "UPDATE DustbinStatus SET Status = 0 WHERE DustbinCode = %s"
+        mycursor.execute(query, (code,))
+        mydb.commit()
+
+        self.on_enter()
+
     def update_edit_layout(self):
         self.ids.edit_tab.clear_widgets()
-        # Add the scroll view to the main layout
-        grid_layout = MDGridLayout(cols=2, spacing=dp(20), padding=dp(20))
+
+        # Create the grid layout for the dustbin data
+        grid_layout = MDGridLayout(cols=2, spacing=dp(20), padding=(dp(40),dp(100),dp(40),dp(40)), size_hint_y=None)
+        grid_layout.bind(minimum_height=grid_layout.setter('height'))
 
         # Get the dustbin data from the database
         query = f"SELECT Dustbin.DustbinCode, Dustbin.DustbinType, Dustbin.Location, DustbinStatus.Status FROM Dustbin " \
@@ -351,7 +411,8 @@ class MainScreen(Screen):
             status = data[3]
 
             # Create the dustbin card
-            dustbin_card = MDCard(orientation='vertical', size_hint=(None, None), size=(dp(250), dp(250)),radius=(25,0,25,0), padding=dp(10))
+            dustbin_card = MDCard(orientation='vertical', size_hint=(None, None), size=(dp(250), dp(250)),
+                                  radius=(25, 0, 25, 0), padding=dp(10))
 
             # Create the dustbin code label
             dustbin_code_label = MDLabel(text=f'Dustbin Code: {dustbin_code}', font_style='Subtitle1', halign='center')
@@ -368,12 +429,15 @@ class MainScreen(Screen):
             # Add the dustbin card to the grid layout
             grid_layout.add_widget(dustbin_card)
 
-        # Add the grid layout to the edit layout
+        # Add the grid layout to the scroll view
         self.edit_scrollview.add_widget(grid_layout)
 
         # Add the scroll view to the main layout
         self.ids.edit_tab.add_widget(self.edit_scrollview)
-        self.ids.edit_tab.add_widget(self.ids.button)
+        fab = MDFloatingActionButton(icon='plus', pos_hint={'x': 0.05, 'y': 0.05})
+        fab.bind(on_release=self.showpopup)
+        self.ids.edit_tab.add_widget(fab)
+
 
 class CleanBeeApp(MDApp):
 
@@ -393,8 +457,10 @@ class CleanBeeApp(MDApp):
         sm.add_widget(MainScreen(name='mainscreen'))
         sm.add_widget(ProfileScreen(name='profilescreen'))
         sm.add_widget(ResetPasswordPage(name='reset_password'))
-
         return sm
+
+end = timer()
+print("Time taken:", end-start)
 
 if __name__ == "__main__":
     CleanBeeApp().run()
